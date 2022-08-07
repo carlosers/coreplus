2c757a3bf4617bd64de593a044c92f4884467075
carlosers <carlosers@gmail.com>
2022-08-07 18:27:01 -0300

    criando dois projetos 1Disciplina e CadernoAmarelo

---
+++ java/CadernoAmarelo/src/oracoes/Main.java
@@ -0,0 +1,11 @@
+package oracoes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String texto = SinalDaCruz.getTexto();
+		System.out.println(texto);
+	}
+
+}
---
+++ java/CadernoAmarelo/src/oracoes/SinalDaCruz.java
@@ -0,0 +1,13 @@
+package oracoes;
+
+public class SinalDaCruz {
+
+	private static String texto="Pelo Sinal da Santa Cruz, livrai-nos, Deus, "
+			+ "nosso Senhor, dos nossos inimigos.\n Em nome do Pai, "
+			+ "do Filho e do Espírito Santo, Amém.";
+	
+	public static String getTexto() {
+		return texto;
+	}
+
+}
